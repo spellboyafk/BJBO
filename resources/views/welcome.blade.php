c7c61ff4958b217a47af0b747ace05c4e1f7deb1
muhammaddanish2110 <muhammaddanish74032@gmail.com>
2025-12-02 16:38:22 +0700

    root panel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -350,7 +350,7 @@
         <div class="row">
           <div class="col-lg-6 pt-5 mt-5">
             <h2 class="display-1 ls-1"><span class="fw-bold text-primary">Organic</span> Foods at your <span class="fw-bold">Doorsteps</span></h2>
-            <p class="fs-4">Dignissim massa diam elementum.</p>
+            <p class="fs-4">MENJUAL ANEKA JENIS BONDA.</p>
             <div class="d-flex gap-3">
               <a href="#" class="btn btn-primary text-uppercase fs-6 rounded-pill px-4 py-3 mt-3">Start Shopping</a>
               <a href="#" class="btn btn-dark text-uppercase fs-6 rounded-pill px-4 py-3 mt-3">Join Now</a>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome'); 
+});
+
+Route::get('/panel', function () {
     return view('admin.dashboard');
 });
