d94119e21d2a05c9f85d51498d17d5596befa46a
MeliSafitri <safitrimeli224@gmail.com>
2025-12-07 13:54:07 +0700

    penambahan route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,27 @@ Route::get('/', function () {
 Route::get('/panel', function () {
     return view('admin.dashboard');
 });
+Route::get('/buah-lokal', function () {
+    return view('buah-lokal');
+})->name('buah.lokal');
+
+Route::get('/buah-impor', function () {
+    return view('buah-impor');
+})->name('buah.impor');
+
+Route::get('/paket-buah', function () {
+    return view('paket-buah');
+})->name('paket.buah');
+
+Route::get('/promo', function () {
+    return view('promo');
+})->name('promo');
+
+Route::get('/mitra-toko', function () {
+    return view('mitra-toko');
+})->name('mitra.toko');
+
+Route::get('/mitra-petani', function () {
+    return view('mitra-petani');
+})->name('mitra.petani');
+
