2cb8bf8881852f5d147e475f0d06ff73a2625b34
richardo <richardochay05@gmail.com>
2026-01-06 12:04:49 +0700

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,22 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-<<<<<<< HEAD
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-=======
-
-    ->withMiddleware(function ($middleware) {
         $middleware->alias([
             'admin' => IsAdmin::class,
         ]);
     })
-
     ->withExceptions(function (Exceptions $exceptions): void {
->>>>>>> 3b1ca24dfc0c2799b402a40dff19dc1006adde2d
         //
-    })->create();
+    })
+    ->create();
